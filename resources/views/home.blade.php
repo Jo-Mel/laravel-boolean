f6ffc2529f586865a7911fc7fd46495fd2696880
Jona M <jonameleqi@gmail.com>
2020-10-12 19:20:56 +0200

    pagina studenti

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section ('titolo', 'home page')
+@section('content')
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, voluptatibus velit magnam soluta optio modi enim quia necessitatibus minus amet fuga, nesciunt, temporibus atque illo fugiat illum et fugit sapiente.    
+    </div>
+@endsection
